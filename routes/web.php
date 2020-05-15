c7e45d5764883812f60a84c4748969eb341e104e
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-05-15 13:29:29 +0100

    Added routes for authentication of users, sponsors and admin.

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,68 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+	Auth::routes();
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+	Route::prefix('user')->group(function () {
+		
+		// @route user/auth
+		// @method GET
+		// @access public
+		// @desc Route to display the users account management login form
+		Route::get('/auth', 'Auth\UserLoginController@showLoginForm')->name('user.auth.show');
+		
+		// @route user/auth
+		// @method POST
+		// @access public
+		// @desc Route to login users to account management section
+		Route::post('/auth', 'Auth\UserLoginController@login')->name('user.auth.submit');
+		
+		// @route user/
+		// @method GET
+		// @access private
+		// @desc Route to display users account management dashboard after authentication
+		Route::get('/', 'UserController@index')->name('user.dashboard');
+	});
+	
+	Route::prefix('sponsor')->group(function () {
+		
+		// @route sponsor/auth
+		// @method GET
+		// @access public
+		// @desc Route to display the sponsors account management login form
+		Route::get('/auth', 'Auth\SponsorLoginController@showLoginForm')->name('sponsor.auth.show');
+		
+		// @route sponsor/auth
+		// @method POST
+		// @access public
+		// @desc Route to login sponsors to account management section
+		Route::post('/auth', 'Auth\SponsorLoginController@login')->name('sponsor.auth.submit');
+		
+		// @route sponsor/
+		// @method GET
+		// @access private
+		// @desc Route to show the account management dashboard for sponsors, after authentication
+		Route::get('/', 'SponsorController@index')->name('sponsor.dashboard');
+	});
+	
+	Route::prefix('admin')->group(function () {
+		// @route admin/login
+		// @method GET
+		// @access public
+		// @desc Route to show the records management backend login form
+		Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login.show');
+		
+		// @route admin/login
+		// @method POST
+		// @access public
+		// @desc Route to login to the records management backend
+		Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+		
+		// @route admin/
+		// @method GET
+		// @access private
+		// @desc Route to display dashboard after user has been authenticated
+		Route::get('/', 'AdminController@index')->name('admin.dashboard');
+	});
+	
+	Route::get('/', 'HomeController@index')->name('home');
