f62f0a5c2255403c5445355cfde21eddd0929c90
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-05-18 16:08:18 +0100

    Initial commit with draft content

--- resources/views/admin/eventsAndNews/home.blade.php
+++ resources/views/admin/eventsAndNews/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Events And News</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    Events and News table
                 </div>
             </div>
         </div>
--- resources/views/admin/sponsors/signage/home.blade.php
+++ resources/views/admin/sponsors/signage/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Sponsors Signage</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    Sponsors signage records table
                 </div>
             </div>
         </div>
