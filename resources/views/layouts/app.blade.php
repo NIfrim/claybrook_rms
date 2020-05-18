f09c98aeaabd7d557b242cec0f65f5c01c2d7484
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-05-18 16:13:01 +0100

    Material icons CDN link added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,10 +18,11 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
 </head>
 <body>
     <div id="app">
-        {{var_dump(Auth::guard('user') -> check())}}
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
