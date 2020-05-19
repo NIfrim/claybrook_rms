d1adcadf2f261d6f2e9303e426009100080ab0ef
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-05-19 16:55:31 +0100

    Added table component

--- resources/views/admin/animals/birds/home.blade.php
+++ resources/views/admin/animals/birds/home.blade.php
@@ -1,23 +1,9 @@
 @extends('layouts.rms')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Birds</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    Birds table
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+	<div class="container">
+		<div class="row justify-content-center">
+			<x-tables.animals-table selectable=true showId=true :columns="$columns" :rows="$rows"/>
+		</div>
+	</div>
 @endsection
