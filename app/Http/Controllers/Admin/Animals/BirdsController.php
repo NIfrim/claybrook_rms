c612b3292307a5298bd82bfab71bd9692acfc436
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-05-19 16:58:37 +0100

    index modified to accommodate table and showForm added

--- app/Http/Controllers/Admin/Animals/BirdsController.php
+++ app/Http/Controllers/Admin/Animals/BirdsController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers\Admin\Animals;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 class BirdsController extends Controller
 {
@@ -24,6 +25,36 @@ class BirdsController extends Controller
 	 */
 	public function index()
 	{
-		return view('admin.animals.birds.home', ['title' => 'Animals - Birds', 'category' => 'animals', 'subcategory' => 'birds']);
+		$animalColumns = Schema::getColumnListing('animals');
+		$birdColumns = Schema::getColumnListing('birds');
+		$columns = array_unique(array_merge($animalColumns, $birdColumns));
+		
+		$rows = DB::table('birds')->join('animals', 'birds.animal_id', '=', 'animals.id')->get()->all();
+		
+		return view('admin.animals.birds.home', [
+			'title' => 'Animals - Birds',
+			'category' => 'animals',
+			'subcategory' => 'birds',
+			'columns' => $columns,
+			'rows' => $rows
+		]);
+	}
+	
+	/**
+	 * Show all the birds records.
+	 *
+	 * @param String $formType
+	 *
+	 * @return \Illuminate\Contracts\Support\Renderable
+	 */
+	public function showAnimalForm(String $formType)
+	{
+		return view('admin.animals.birds.forms', [
+			'title' => 'Animals - Birds',
+			'category' => 'animals',
+			'subcategory' => 'birds',
+			'formType' => $formType,
+			'animalType' => 'bird'
+		]);
 	}
 }
