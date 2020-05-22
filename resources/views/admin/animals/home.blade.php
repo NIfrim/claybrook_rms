104a532a22d582af3b5f1c90f6785af0e621d72d
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-05-22 17:21:20 +0100

    Minor changes

--- resources/views/admin/animals/home.blade.php
+++ resources/views/admin/animals/home.blade.php
@@ -3,11 +3,7 @@
 @section('content')
 	<div class="container">
 		<div class="row justify-content-center">
-			{{--<x-tables.animals-table--}}
-					{{--selectable=true--}}
-					{{--:model="$model"--}}
-					{{--:relations="$relations"--}}
-			{{--/>--}}
+			<x-tables.animals-table selectable=true :model="$model" :relations="$relations" />
 		</div>
 	</div>
 @endsection
