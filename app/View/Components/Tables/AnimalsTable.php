b9ffc1dfa6ac7bd1ccc7f905e2833d479c844a8f
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-05-22 17:19:21 +0100

    Initial commit

--- app/View/Components/Tables/AnimalsTable.php
+++ app/View/Components/Tables/AnimalsTable.php
@@ -2,27 +2,31 @@
 
 namespace App\View\Components\Tables;
 
-use Illuminate\View\Component;
+use App\View\Components\DefaultTable;
 
-class AnimalsTable extends Component
+class AnimalsTable extends DefaultTable
 {
-    /**
-     * Create a new component instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|string
-     */
-    public function render()
-    {
-        return view('components.tables.animals-table');
-    }
+	/**
+	 * Get the view / contents that represent the component.
+	 *
+	 * @return \Illuminate\View\View|string
+	 */
+	public function render()
+	{
+		$rows = $this->getRows(10);
+		
+		return view('components.tables.animals-table', [
+			'columns' => [
+				['name' => 'id', 'title' => 'id', 'type' => 'text'],
+				['name' => 'location_id', 'title' => 'location', 'type' => 'text'],
+				['name' => 'classification', 'title' => 'classification', 'type' => 'text'],
+				['name' => 'name', 'title' => 'name', 'type' => 'text'],
+				['name' => 'diet', 'title' => 'diet', 'type' => 'text'],
+				['name' => 'dob', 'title' => 'date of birth', 'type' => 'date'],
+				['name' => 'sponsorship_band_id', 'title' => 'sponsorship band', 'type' => 'text'],
+			],
+			'rows' => $rows,
+			'params' => request()->except('page'),
+		]);
+	}
 }
