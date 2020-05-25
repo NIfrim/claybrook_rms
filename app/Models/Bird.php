9b7b260b35a6325cc50e976a1f8fcceaf981a6a8
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-05-25 16:41:39 +0100

    Added fillables

--- app/Models/Bird.php
+++ app/Models/Bird.php
@@ -4,5 +4,26 @@ namespace App\Models;
 
 class Bird extends Animal
 {
-	// Code
+	protected $fillable = [
+		'id',
+		'location_id',
+		'sponsorship_band_id',
+		'species',
+		'classification',
+		'type',
+		'name',
+		'date_joined',
+		'dob',
+		'gender',
+		'height_joined',
+		'weight_joined',
+		'nest_construction',
+		'clutch_size',
+		'wingspan',
+		'can_fly',
+		'plumage',
+		'diet',
+		'created_at',
+		'updated_at'
+	];
 }
