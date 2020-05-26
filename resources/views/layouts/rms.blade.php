3e1ef5635d78c5244b86692ae17ab3302a7c4d90
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-05-26 16:37:46 +0100

    success alert added

--- resources/views/layouts/rms.blade.php
+++ resources/views/layouts/rms.blade.php
@@ -33,6 +33,14 @@
             {{--TOP NAV--}}
             <x-navigation.top-nav :category="$category" :subcategory="$subcategory" />
             <main id="main" class="container-fluid overflow-auto">
+                @if(session('success'))
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        <strong>{{session('success')}}</strong>
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                @endif
                 @yield('content')
             </main>
         </div>
