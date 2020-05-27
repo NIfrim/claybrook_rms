df1700c3995994d2f0e37bb33e0846bcf108ea76
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-05-27 16:50:55 +0100

    Made dynamic to be used by all related subsections

--- resources/views/admin/animals/birds/forms.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.rms')
-
-@section('content')
-	<div class="container forms-container">
-		<x-forms.animal-form :data="$data" :formType="$formType" :category="$category" :subcategory="$subcategory" title="Animals - Birds - Manage" />
-	</div>
-@endsection
