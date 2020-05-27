deb635925c418f8bf8f968687c67a969c2e3bd4b
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-05-27 16:51:32 +0100

    Replaced with only one dynamic file

--- resources/views/admin/locations/aviary/home.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.rms')
-
-@section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <x-tables.default-table selectable=true showId=true :columns="$columns" :rows="$aviariesWithRelated"/>
-        </div>
-    </div>
-@endsection
