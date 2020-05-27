224239c1629a5acf034e517991e2105598128b50
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-05-27 16:53:47 +0100

    Added fillable

--- app/Models/Mammal.php
+++ app/Models/Mammal.php
@@ -4,5 +4,24 @@ namespace App\Models;
 
 class Mammal extends Animal
 {
-	// Code
+	protected $fillable = [
+		'id',
+		'location_id',
+		'sponsorship_band_id',
+		'species',
+		'classification',
+		'type',
+		'name',
+		'date_joined',
+		'dob',
+		'gender',
+		'height_joined',
+		'weight_joined',
+		'gestational_period',
+		'offspring_number',
+		'colour',
+		'diet',
+		'created_at',
+		'updated_at'
+	];
 }
--- app/Models/Reptile.php
+++ app/Models/Reptile.php
@@ -4,5 +4,24 @@ namespace App\Models;
 
 class Reptile extends Animal
 {
-	// Code
+	protected $fillable = [
+		'id',
+		'location_id',
+		'sponsorship_band_id',
+		'species',
+		'classification',
+		'type',
+		'name',
+		'date_joined',
+		'dob',
+		'gender',
+		'height_joined',
+		'weight_joined',
+		'reproduction_type',
+		'offspring_number',
+		'clutch_size',
+		'diet',
+		'created_at',
+		'updated_at'
+	];
 }
