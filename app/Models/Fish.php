62ec1dae0552dcc849422f9ecced622ff246d170
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-05-27 16:49:09 +0100

    Added fillable

--- app/Models/Fish.php
+++ app/Models/Fish.php
@@ -4,5 +4,24 @@ namespace App\Models;
 
 class Fish extends Animal
 {
-	// Code
+	protected $fillable = [
+		'id',
+		'location_id',
+		'sponsorship_band_id',
+		'species',
+		'classification',
+		'type',
+		'name',
+		'date_joined',
+		'dob',
+		'gender',
+		'height_joined',
+		'weight_joined',
+		'average_body_temperature',
+		'water_type',
+		'colour',
+		'diet',
+		'created_at',
+		'updated_at'
+	];
 }
