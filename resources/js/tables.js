9cef76d2ed50763845072f0d9edb436880f30122
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-05-28 17:01:03 +0100

    table2 added as datatable

--- resources/js/tables.js
+++ resources/js/tables.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-	const table = $('#table').DataTable({
+	const table = $('#table, #table2').DataTable({
 		select: true,
 		language: {
 			emptyTable: "No data available in table"
