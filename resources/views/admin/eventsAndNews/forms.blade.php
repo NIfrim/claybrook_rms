58d23f144237f8a80302df5757ffdc68d6467d15
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-05-28 16:59:46 +0100

    Minor changes

--- resources/views/admin/eventsAndNews/forms.blade.php
+++ resources/views/admin/eventsAndNews/forms.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 	<div class="container forms-container">
 		@if($formType === 'newCategory')
-			<x-forms.events-and-news-category-form :data="$data" :formType="$formType" :category="$category" :subcategory="$subcategory" title="{{implode(' - ', array_filter([ucfirst($subcategory), 'Manage']))}}" />
+			<x-forms.events-and-news-category-form :data="$data" :formType="$formType" :category="$category" :subcategory="$subcategory" title="{{implode(' - ', array_filter([ucfirst($subcategory), 'Category', 'Manage']))}}" />
 		@else
 			<x-forms.events-and-news-form :data="$data" :formType="$formType" :category="$category" :subcategory="$subcategory" title="{{implode(' - ', array_filter([ucfirst($subcategory), 'Manage']))}}" />
 		@endif
