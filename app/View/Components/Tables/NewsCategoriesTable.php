8f2a4ee72bf911ffe3d1b1d49cc5922b8d77a8d9
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-05-28 17:00:39 +0100

    Initial commit

---
+++ app/View/Components/Tables/NewsCategoriesTable.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\View\Components\Tables;
+
+use App\View\Components\DefaultTable;
+
+class NewsCategoriesTable extends DefaultTable
+{
+	/**
+	 * Get the view / contents that represent the component.
+	 *
+	 * @return \Illuminate\View\View|string
+	 */
+	public function render()
+	{
+		$rows = $this->getRows(null);
+		$columns = $this->getColumns();
+		
+		return view('components.tables.news-categories-table', [
+			'category' => $this->category,
+			'subcategory' => $this->subcategory,
+			'rows' => $rows,
+			'columns' => $columns,
+		]);
+	}
+	
+	protected function getColumns()
+	{
+		$columns = [
+			['name' => 'id', 'title' => 'id', 'type' => 'number'],
+			['name' => 'title', 'title' => 'title', 'type' => 'text'],
+			['name' => 'short_description', 'title' => 'short description', 'type' => 'text'],
+			['name' => 'long_description', 'title' => 'long description', 'type' => 'text'],
+		];
+		
+		return $columns;
+	}
+}
