4756c8aa78b0817b7ed50ea59d014f2edf8b033c
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-05-28 14:29:01 +0100

    Changed to contain title only

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <x-tables.locations-table selectable=true :model="$model" :relations="$relations" :category="$category" :subcategory="$subcategory" />
+            <h1>Dashboard</h1>
         </div>
     </div>
 @endsection
