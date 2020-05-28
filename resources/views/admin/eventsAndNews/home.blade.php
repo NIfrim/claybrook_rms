d8a7e737fd3301ff810cb1118c9d8c497dc7c1c6
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-05-28 17:00:07 +0100

    Added tabbed panes

--- resources/views/admin/eventsAndNews/home.blade.php
+++ resources/views/admin/eventsAndNews/home.blade.php
@@ -2,31 +2,35 @@
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
+        <div class="container">
           <ul class="nav nav-tabs" role="tablist">
-            <li class="nav-item" role="menuitem">
-              <a href="{{route('admin.eventsAndNews.list', ['type' => $subcategory])}}" class="nav-link {{$subcategory === 'events' | $subcategory === 'news' ? 'active' : ''}}">{{ucfirst($subcategory)}} list</a>
+            <li class="nav-item">
+              <a href="{{route('admin.eventsAndNews.list', ['type' => $subcategory])}}" class="nav-link {{isset($subcategory2) ? '' : 'active'}}">{{ucfirst($subcategory)}} list</a>
             </li>
-            <li class="nav-item" role="menuitem">
-              <a href="{{route('admin.eventsAndNews.list', ['type' => $subcategory.'Categories'])}}" class="nav-link {{$subcategory === 'categories' ? 'active' : ''}}">{{ucfirst($subcategory)}} categories</a>
+            <li class="nav-item">
+              <a href="{{route('admin.eventsAndNews.list.categories', ['type' => $subcategory])}}" class="nav-link {{isset($subcategory2) ? 'active' : ''}}">{{ucfirst($subcategory)}} categories</a>
             </li>
           </ul>
           
-          <div class="tab-content">
-            <div class="tab-pane {{$subcategory === 'events' | $subcategory === 'news' ? 'active' : ''}}" role="tabpanel">
+          <div class="tab-content p-4 border-left border-right border-bottom">
+            
+            <div class="tab-pane {{isset($subcategory2) ? '' : 'active'}}" id="subcategory" role="tabpanel">
               @if($subcategory === 'events')
                 <x-tables.events-table selectable=true :model="$model" :relations="$relations" :category="$category" :subcategory="$subcategory" />
               @elseif($subcategory === 'news')
                 <x-tables.news-table selectable=true :model="$model" :relations="$relations" :category="$category" :subcategory="$subcategory" />
               @endif
+              
             </div>
             
-            <div class="tab-pane" role="tabpanel">
-              Categories list
+            <div class="tab-pane {{isset($subcategory2) ? 'active' : ''}}" id="categories" role="tabpanel">
+              @if($subcategory === 'events')
+                <x-tables.events-categories-table selectable=true :model="$model" :relations="$relations" :category="$category" :subcategory="$subcategory" />
+              @elseif($subcategory === 'news')
+                <x-tables.news-categories-table selectable=true :model="$model" :relations="$relations" :category="$category" :subcategory="$subcategory" />
+              @endif
             </div>
           </div>
-          
-          
         </div>
     </div>
 @endsection
