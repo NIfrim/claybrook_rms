2fca746d651daa1947f8ebfdd6a2e0e8594e2542
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-05-28 14:28:47 +0100

    Initial commit, testing tabbed panes

--- resources/views/admin/eventsAndNews/home.blade.php
+++ resources/views/admin/eventsAndNews/home.blade.php
@@ -1,23 +1,32 @@
 @extends('layouts.rms')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Events And News</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    Events and News table
-                </div>
+    <div class="container">
+        <div class="row justify-content-center">
+          <ul class="nav nav-tabs" role="tablist">
+            <li class="nav-item" role="menuitem">
+              <a href="{{route('admin.eventsAndNews.list', ['type' => $subcategory])}}" class="nav-link {{$subcategory === 'events' | $subcategory === 'news' ? 'active' : ''}}">{{ucfirst($subcategory)}} list</a>
+            </li>
+            <li class="nav-item" role="menuitem">
+              <a href="{{route('admin.eventsAndNews.list', ['type' => $subcategory.'Categories'])}}" class="nav-link {{$subcategory === 'categories' ? 'active' : ''}}">{{ucfirst($subcategory)}} categories</a>
+            </li>
+          </ul>
+          
+          <div class="tab-content">
+            <div class="tab-pane {{$subcategory === 'events' | $subcategory === 'news' ? 'active' : ''}}" role="tabpanel">
+              @if($subcategory === 'events')
+                <x-tables.events-table selectable=true :model="$model" :relations="$relations" :category="$category" :subcategory="$subcategory" />
+              @elseif($subcategory === 'news')
+                <x-tables.news-table selectable=true :model="$model" :relations="$relations" :category="$category" :subcategory="$subcategory" />
+              @endif
             </div>
+            
+            <div class="tab-pane" role="tabpanel">
+              Categories list
+            </div>
+          </div>
+          
+          
         </div>
     </div>
-</div>
 @endsection
