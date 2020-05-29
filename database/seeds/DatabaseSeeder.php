319710b5fa843cfb980878cbdfc565cca7c76034
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-05-29 16:17:14 +0100

    Seeder for SponsorAgreement

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
 //	 	$this->call(AccountTypeSeeder::class);
 //		$this->call(EmployeeSeeder::class);
 //		$this->call(SponsorSeeder::class);
-		$this->call(UserSeeder::class);
+		$this->call(SponsorAgreementSeeder::class);
+//		$this->call(UserSeeder::class);
     }
 }
