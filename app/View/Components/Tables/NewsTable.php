0cbfab21ce20bb6c424353159f77f1e22c20b966
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-05-29 16:21:21 +0100

    Initial commit

--- app/View/Components/Tables/NewsTable.php
+++ app/View/Components/Tables/NewsTable.php
@@ -2,27 +2,42 @@
 
 namespace App\View\Components\Tables;
 
-use Illuminate\View\Component;
+use App\View\Components\DefaultTable;
 
-class NewsTable extends Component
+class NewsTable extends DefaultTable
 {
-    /**
-     * Create a new component instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\View\View|string
-     */
-    public function render()
-    {
-        return view('components.tables.news-table');
-    }
+	/**
+	 * Get the view / contents that represent the component.
+	 *
+	 * @return \Illuminate\View\View|string
+	 */
+	public function render()
+	{
+		$rows = $this->getRows(null);
+		$columns = $this->getColumns();
+		
+		return view('components.tables.news-table', [
+			'category' => $this->category,
+			'subcategory' => $this->subcategory,
+			'rows' => $rows,
+			'columns' => $columns,
+		]);
+	}
+	
+	protected function getColumns()
+	{
+		$columns = [
+			['name' => 'id', 'title' => 'id', 'type' => 'number'],
+			['name' => 'zoo_id', 'title' => 'zoo id', 'type' => 'number'],
+			['name' => 'category_id', 'title' => 'category id', 'type' => 'number'],
+			['name' => 'title', 'title' => 'title', 'type' => 'text'],
+			['name' => 'date_posted', 'title' => 'starts', 'type' => 'date'],
+			['name' => 'date_expires', 'title' => 'ends', 'type' => 'date'],
+			['name' => 'repeat', 'title' => 'repeats', 'type' => 'text'],
+			['name' => 'short_description', 'title' => 'short description', 'type' => 'text'],
+			['name' => 'long_description', 'title' => 'long description', 'type' => 'text'],
+		];
+		
+		return $columns;
+	}
 }
