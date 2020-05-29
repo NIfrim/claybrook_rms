66ce9e432ec473e6f11ab6c0b98da3e08c7c861f
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-05-29 16:20:40 +0100

    minor change to display correct forms

--- resources/views/admin/eventsAndNews/forms.blade.php
+++ resources/views/admin/eventsAndNews/forms.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 	<div class="container forms-container">
-		@if($formType === 'newCategory')
+		@if($formType === 'newCategory' || $subcategory === 'eventsCategory' || $subcategory === 'newsCategory')
 			<x-forms.events-and-news-category-form :data="$data" :formType="$formType" :category="$category" :subcategory="$subcategory" title="{{implode(' - ', array_filter([ucfirst($subcategory), 'Category', 'Manage']))}}" />
 		@else
 			<x-forms.events-and-news-form :data="$data" :formType="$formType" :category="$category" :subcategory="$subcategory" title="{{implode(' - ', array_filter([ucfirst($subcategory), 'Manage']))}}" />
