1596d81795d9968c504adb41a6efe4705774e283
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-05-29 16:20:53 +0100

    Initial commit

---
+++ resources/views/admin/sponsors/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.rms')
+
+@section('content')
+  <div class="container">
+    <div class="row justify-content-center">
+      
+      @if($subcategory === 'accounts')
+
+        <x-tables.sponsors-accounts-table selectable=true :model="$model" :relations="$relations" :category="$category" :subcategory="$subcategory" />
+        
+      @elseif($subcategory === 'agreements')
+    
+        <x-tables.sponsors-agreements-table selectable=true :model="$model" :relations="$relations" :category="$category" :subcategory="$subcategory" />
+        
+      @elseif($subcategory === 'signage')
+    
+        <x-tables.agreements-signage-table selectable=true :model="$model" :relations="$relations" :category="$category" :subcategory="$subcategory" />
+        
+      @endif
+      
+    </div>
+  </div>
+@endsection
