0233d8f45b031d6846dbb184090aa56436c39f1e
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-05-29 16:19:00 +0100

    Fixed action btn roundness

--- resources/views/components/tables/locations-table.blade.php
+++ resources/views/components/tables/locations-table.blade.php
@@ -26,7 +26,7 @@
                         @csrf
                         <input type = "text" name="ids" value="{{$location->id}}" hidden />
                         
-                        <button type="submit" class="icon-btn">
+                        <button type="submit" class="icon-btn p-0">
                             <i class="remove material-icons p-2 deleteRow" title="Delete">delete</i>
                         </button>
                     
