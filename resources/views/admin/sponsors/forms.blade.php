28d01f21865729e3d9b2f5da32acee8cb05692b0
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-05-30 14:14:35 +0100

    Signage forms added

--- resources/views/admin/sponsors/forms.blade.php
+++ resources/views/admin/sponsors/forms.blade.php
@@ -12,7 +12,7 @@
 				@break
 		
 			@case('signage')
-		
+			<x-forms.agreement-signage-form :data="$data" :formType="$formType" :category="$category" :subcategory="$subcategory" title="{{implode(' - ', array_filter([ucfirst($category), ucfirst($subcategory), 'Manage']))}}" />
 				@break
 			
 			@default
