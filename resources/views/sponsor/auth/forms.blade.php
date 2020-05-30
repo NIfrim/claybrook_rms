7ee42ef3f620869f0984ab6791716f05ba17318e
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-05-30 16:04:44 +0100

    Moved location

--- resources/views/sponsor/auth/forms.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="container">
-        <x-web-login-form :loginRoute="$loginRoute"/>
-        <x-sponsor-registration-form :registerRoute="$registerRoute"/>
-    </div>
-@endsection
--- resources/views/user/auth/forms.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="container">
-        <x-web-login-form :loginRoute="$loginRoute"/>
-        <x-user-registration-form :registerRoute="$registerRoute"/>
-    </div>
-@endsection
