44361c5b4e318c555f21e9724dd2c93b989a2527
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-05-30 14:15:36 +0100

    Added fillable

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -6,6 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
+	protected $fillable = [
+		'zoo_id',
+		'sponsor_id',
+		'user_id',
+		'posted_by',
+		'title',
+		'review',
+		'reaction',
+		'posted'
+	];
+	
 	public function user() {
 		return $this->belongsTo('App\Models\User');
 	}
