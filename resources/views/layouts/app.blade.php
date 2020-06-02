6197ce2c1fef6a63820ae02c5b6ef235900c5bec
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-06-02 16:55:48 +0100

    Minor changes including footer and carousel, in progress

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,9 +8,10 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-
+    
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ mix('js/app.js') }}" defer></script>
+    <script src="{{ mix('css/app.css') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -22,68 +23,17 @@
 
 </head>
 <body>
-    <div id="app">
+    <div id="app" class="d-flex flex-column">
+        <header class="d-flex justify-content-center flex-column">
+            <x-website.navigation.navbar :category="$category" :subcategory="$subcategory" />
+        </header>
         
-        <x-website.navigation.navbar :category="$category" :subcategory="$subcategory" />
-        {{--<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">--}}
-            {{--<div class="container">--}}
-                {{--<a class="navbar-brand" href="{{ url('/') }}">--}}
-                    {{--{{ config('app.name', 'Laravel') }}--}}
-                {{--</a>--}}
-                {{--<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">--}}
-                    {{--<span class="navbar-toggler-icon"></span>--}}
-                {{--</button>--}}
-
-                {{--<div class="collapse navbar-collapse" id="navbarSupportedContent">--}}
-                    {{--<!-- Left Side Of Navbar -->--}}
-                    {{--<ul class="navbar-nav mr-auto">--}}
-
-                    {{--</ul>--}}
-
-                    {{--<!-- Right Side Of Navbar -->--}}
-                    {{--<ul class="navbar-nav ml-auto">--}}
-                        {{--<!-- Authentication Links -->--}}
-                        {{--@guest--}}
-                            {{--<li class="nav-item dropdown">--}}
-                                {{--<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
-                                    {{--Account--}}
-                                {{--</button>--}}
-                                {{--<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">--}}
-                                    {{--@if(Route::has('user.home'))--}}
-                                        {{--<a class="nav-link" href="{{ route('user.home') }}">{{ __('User') }}</a>--}}
-                                    {{--@endif--}}
-
-                                    {{--@if(Route::has('sponsor.home'))--}}
-                                        {{--<a class="nav-link" href="{{ route('sponsor.home') }}">{{ __('Sponsor') }}</a>--}}
-                                    {{--@endif--}}
-                                {{--</div>--}}
-                            {{--</li>--}}
-                        {{--@else--}}
-                            {{--<li class="nav-item dropdown">--}}
-                                {{--<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>--}}
-                                    {{--{{ Auth::user()->first_name }} <span class="caret"></span>--}}
-                                {{--</a>--}}
-
-                                {{--<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">--}}
-                                    {{--<a class="dropdown-item" href="{{ route('user.logout') }}"--}}
-                                       {{--onclick="event.preventDefault();--}}
-                                                     {{--document.getElementById('logout-form').submit();">--}}
-                                        {{--{{ __('Logout') }}--}}
-                                    {{--</a>--}}
-
-                                    {{--<form id="logout-form" action="{{ route('user.logout') }}" method="POST" style="display: none;">--}}
-                                        {{--@csrf--}}
-                                    {{--</form>--}}
-                                {{--</div>--}}
-                            {{--</li>--}}
-                        {{--@endguest--}}
-                    {{--</ul>--}}
-                {{--</div>--}}
-            {{--</div>--}}
-        {{--</nav>--}}
-
-        <main class="py-4">
+        <main>
             @yield('content')
+    
+            <footer>
+                <x-website.footer :zoo="$zoo" />
+            </footer>
         </main>
     </div>
 </body>
