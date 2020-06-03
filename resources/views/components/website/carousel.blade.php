8aaa33456a2e7be1358ef4dacd74386b03c7f4c0
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-06-03 17:15:56 +0100

    Made dynamic

--- resources/views/components/website/carousel.blade.php
+++ resources/views/components/website/carousel.blade.php
@@ -1,26 +1,41 @@
 <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
     <div class="carousel-inner">
-        <div class="carousel-item active">
-            <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: First slide">
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#faffd6"/>
-                <text font-size="3rem" x="50%" y="50%" fill="#555" dy="0">First slide</text>
-            </svg>
-        </div>
-        <div class="carousel-item">
-            <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Second Slide">
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#faffd6"/>
-                <text font-size="3rem" x="50%" y="50%" fill="#555" dy="0">Second slide</text>
-            </svg>
-        </div>
-        <div class="carousel-item">
-            <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Third slide">
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#faffd6"/>
-                <text font-size="3rem" x="50%" y="50%" fill="#555" dy="0">Third slide</text>
-            </svg>
-        </div>
+        @if($images)
+        
+            @foreach($images as $image)
+            
+                <div class="carousel-item active">
+                    <img src = "{{$image}}" alt = "Carousel image" width="100%" height="auto" class="img-fluid">
+                </div>
+            
+            @endforeach
+        
+        @else
+        
+            <div class="carousel-item active">
+                <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: First slide">
+                    <title>Placeholder</title>
+                    <rect width="100%" height="100%" fill="#faffd6"/>
+                    <text font-size="3rem" x="50%" y="50%" fill="#555" dy="0">First slide</text>
+                </svg>
+            </div>
+            <div class="carousel-item">
+                <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Second Slide">
+                    <title>Placeholder</title>
+                    <rect width="100%" height="100%" fill="#faffd6"/>
+                    <text font-size="3rem" x="50%" y="50%" fill="#555" dy="0">Second slide</text>
+                </svg>
+            </div>
+            <div class="carousel-item">
+                <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Third slide">
+                    <title>Placeholder</title>
+                    <rect width="100%" height="100%" fill="#faffd6"/>
+                    <text font-size="3rem" x="50%" y="50%" fill="#555" dy="0">Third slide</text>
+                </svg>
+            </div>
+            
+        @endif
+        
     </div>
     <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
