0ffff12ad248b68c644085a7a21825ba851c1200
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-06-03 17:14:27 +0100

    Linked to route

--- resources/views/components/website/animal-card.blade.php
+++ resources/views/components/website/animal-card.blade.php
@@ -1,7 +1,9 @@
-<div class="card animal-card">
-    <div class="card-body">
-        <div class="card-title">{{$title}}</div>
-        <div class="card-sponsor">Sponsored by: {{$sponsor['fullName']}}}}</div>
-        <img src = "{{$image}}" alt = "Animal Image">
+<div class="card flex-shrink-0 m-2 p-0 rounded-lg shadow-sm animal-card">
+    <div class="card-header">
+        <h5 class="card-title">{{$title}}</h5>
+        <p class="card-sponsor">Sponsored by: <small class="text-muted">{{$sponsor}}</small> </p>
     </div>
+    <a href = "{{route('website.animals.showOne', ['type' => $type, 'id' => $id])}}">
+        <img src = "{{$image ?? 'https://via.placeholder.com/250x250'}}" alt = "Animal Image">
+    </a>
 </div>
