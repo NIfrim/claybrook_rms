130b2d026e1625233f79672a85488cb70ccdc16e
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-06-03 17:15:01 +0100

    Required updates

--- app/View/Components/Website/AnimalCard.php
+++ app/View/Components/Website/AnimalCard.php
@@ -6,20 +6,22 @@ use Illuminate\View\Component;
 
 class AnimalCard extends Component
 {
-	public $title, $sponsor, $image;
+	public $id, $title, $sponsor, $image, $type, $data;
 	
 	/**
 	 * Create a new component instance.
 	 *
+	 * @param string $type
 	 * @param string $title
-	 * @param array $sponsor
-	 * @param string $image
+	 * @param object $data
 	 */
-	public function __construct(string $title, array $sponsor, string $image)
+	public function __construct(string $type, string $title, object $data)
 	{
+		$this->id = $data->id;
 		$this->title = $title;
-		$this->sponsor = $sponsor;
-		$this->image = $image;
+		$this->sponsor = isset($data->sponsor) ? $data->sponsor->first_name.' - '.$data->sponsor->last_name : 'No Sponsor';
+		$this->image = $data->image ?? null;
+		$this->type = $type;
 	}
 	
 	/**
