84f2d87f18ac189a6d264dbf077681234c8f03b5
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-06-04 15:38:47 +0100

    Reduced margin to text

--- resources/views/components/website/animal-card.blade.php
+++ resources/views/components/website/animal-card.blade.php
@@ -1,7 +1,7 @@
 <div class="card flex-shrink-0 m-2 p-0 rounded-lg shadow-sm animal-card">
     <div class="card-header">
-        <h5 class="card-title">{{$title}}</h5>
-        <p class="card-sponsor">Sponsored by: <small class="text-muted">{{$sponsor}}</small> </p>
+        <h5 class="card-title mb-1">{{$title}}</h5>
+        <p class="card-sponsor mb-1">Sponsored by: <small class="text-muted">{{$sponsor}}</small> </p>
     </div>
     <a href = "{{route('website.animals.showOne', ['type' => $type, 'id' => $id])}}">
         <img src = "{{$image ?? 'https://via.placeholder.com/250x250'}}" alt = "Animal Image">
