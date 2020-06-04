6ce19bb7007f3458003b8d65acc226efae7f9d64
Nicolae Ifrim <ifrim.madalin@gmail.com>
2020-06-04 15:45:27 +0100

    Added events and news cards

--- resources/views/components/website/row-type-one.blade.php
+++ resources/views/components/website/row-type-one.blade.php
@@ -1,29 +1,55 @@
 <div class="row-type-one">
-	<div class="row-top">
+	<div class="row-body">
 		<div class="container d-flex justify-content-center">
 			{{--Title--}}
 			<h2 class="row-title my-3">{{$title}}</h2>
 		</div>
-	</div>
-	<div class="row-bottom">
-		<div class="container d-flex flex-column align-items-center justify-content-between">
-			@if(sizeof($cardsData) > 0)
+		
+		<div class="row-cards">
+			<div class="container d-flex flex-wrap align-items-center justify-content-center">
+				@if(sizeof($cardsData) > 0)
+					
+					@foreach($cardsData as $card)
+						
+						@if($card->getTable() === 'events')
+							
+							<x-website.event-card
+									:title="$card['title']"
+									:startDate="$card['start_date']"
+									:endDate="$card['end_date']"
+									:image="$card['image']"
+									:shortDescription="$card['short_description']"
+							/>
+						
+						@elseif($card->getTable() === 'news')
+							
+							<x-website.news-card
+									:title="$card['title']"
+									:datePosted="$card['date_posted']"
+									:dateExpire="$card['date_expire']"
+									:image="$card['image']"
+									:shortDescription="$card['short_description']"
+							/>
+						
+						@elseif($card->getTable() === 'animals')
+							
+							<x-website.animal-card
+									:title="$card->species.' - '.$card->name"
+									:data="$card"
+									:type="$type"
+							/>
+						
+						@endif
+					
+					
+					@endforeach
 				
-				@foreach($cardsData as $card)
-					<x-website.event-card
-							:title="$card['title']"
-							:startDate="$card['start_date']"
-							:endDate="$card['end_date']"
-							:image="$card['image']",
-							:shortDescription="$card['short_description']"
-					/>
-				@endforeach
+				@else
+					
+					<h4>No data for {{$title}}</h4>
 				
-			@else
-				
-				<h4>No data for {{$title}}</h4>
-				
-			@endif
+				@endif
+			</div>
 		</div>
 	</div>
 </div>
