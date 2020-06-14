eb7099c9a535c10a8fa419e786f865646240a479
Madalin <ifrim.madalin@gmail.com>
2020-06-14 15:30:35 +0100

    Update rms.blade.php

--- resources/views/layouts/rms.blade.php
+++ resources/views/layouts/rms.blade.php
@@ -38,7 +38,7 @@
 		<div class="flex-grow-1 d-flex flex-column">
 			{{--TOP NAV--}}
 			<x-navigation.top-nav :category="$category" :subcategory="$subcategory" :subcategory2="$subcategory2 ?? null" :formType="$formType ?? null" />
-			<main id="main" class="container-fluid overflow-auto">
+			<main id="main" class="container overflow-auto">
 				@yield('content')
 				
 				@if(session('success'))
